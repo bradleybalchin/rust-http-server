a0ceee72aee3b54c2f8656c50ebfe064f5d46818
Bradley Balchin <bradleybalchin@hotmail.co.uk>
2026-07-29 00:11:28 +0100

    basic webserver with axum

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Router,
+};
+use tower_http::services::{ServeDir, ServeFile};
+
+
+#[tokio::main]
+async fn main() {
+    // router for webserver
+    let app = Router::new().fallback_service(
+    ServeDir::new("public")
+        .not_found_service(ServeFile::new("public/404.html")),
+    );
+        
+
+
+
+    // run webserver with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
