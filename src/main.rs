6025304b682275d325baae1ec45687b6b28a79f7
Bradley Balchin <bradleybalchin@hotmail.co.uk>
2026-08-03 01:26:16 +0100

    comment and format change

--- src/main.rs
+++ src/main.rs
@@ -39,35 +39,28 @@ async fn main() {
 
 
     //let admin_protected_routes = Router::new();
-    //let user_protected_routes = Router::new();
 
     //must be logged in as user 
     let user_protected_routes = Router::<AppState>::new()
-    .route("/", get(handlers::home::index))     //index
-
-    .nest("/api/files", api::files::router())     //file api
-
-    .layer(axum_middleware::from_fn(middleware::user::auth_user));     // user authenitcation middleware
+        .route("/", get(handlers::home::index))     //index
+        .nest("/api/files", api::files::router())     //file api
+        .layer(axum_middleware::from_fn(middleware::user::auth_user));     // user authenitcation middleware
 
     // router for webserver
     let app = Router::<AppState>::new()
-    //auth
-    .route("/login", get(handlers::auth::login_page)
-                                        .post(handlers::auth::login))     
-    .route("/logout", post(handlers::auth::logout))
+        .route("/login", get(handlers::auth::login_page))//auth                                  .post(handlers::auth::login))     
+        .route("/logout", post(handlers::auth::logout))
+        .merge(user_protected_routes)
 
-    .merge(user_protected_routes)
-
-    //404 fallback
-    .fallback_service(
-    ServeDir::new("public")
-        .not_found_service(ServeFile::new("public/404.html")),
-    )
-    .layer(CookieManagerLayer::new())
-    .with_state(state);
+        .fallback_service(
+        ServeDir::new("public")
+            .not_found_service(ServeFile::new("public/404.html")),    //404 fallback
+        )
+        .layer(CookieManagerLayer::new())
+        .with_state(state); //database connection
         
 
-    // run webserver
+    // run webserver using tokio async runtime
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
